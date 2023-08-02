3c2e72a5f790b0f7ea2c5f80ec3ca2ef049d87d9
eesa456 <eesa.mahmood1@nhs.net>
2023-08-02 17:17:14 +0100

    enable key rotation

--- modules/opennext-revalidation-queue/kms.tf
+++ modules/opennext-revalidation-queue/kms.tf
@@ -10,6 +10,7 @@ resource "aws_kms_key" "revalidation_queue_key" {
   deletion_window_in_days = 10
 
   policy = data.aws_iam_policy_document.revalidation_queue_key_policy[0].json
+  enable_key_rotation = true
 }
 
 data "aws_iam_policy_document" "revalidation_queue_key_policy" {
