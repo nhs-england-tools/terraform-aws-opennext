802e899cf90448d6d90288af9aecc0ba63e5874e
Sandy Forrester <sandy.forrester1@nhs.net>
2026-09-23 09:16:36 +0100

    SPINECI-2479 Bump grype image ver

--- modules/cloudfront-logs/s3__logs.tf
+++ modules/cloudfront-logs/s3__logs.tf
@@ -93,6 +93,8 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "logs" {
       kms_master_key_id = var.log_bucket_kms_key_arn
       sse_algorithm     = "aws:kms"
     }
+
+    bucket_key_enabled = true
   }
 }
 
